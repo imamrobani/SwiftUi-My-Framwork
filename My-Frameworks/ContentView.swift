e73c41809571cb552b4f745521cd054da22ddad5
Imam Robani <imamrob@gmail.com>
2023-09-18 05:45:17 +0700

    Initial Commit

---
+++ My-Frameworks/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  My-Frameworks
+//
+//  Created by Imam Robani on 18/09/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ My-Frameworks/My_FrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_FrameworksApp.swift
+//  My-Frameworks
+//
+//  Created by Imam Robani on 18/09/23.
+//
+
+import SwiftUI
+
+@main
+struct My_FrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
